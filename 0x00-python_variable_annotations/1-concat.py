0868fb498f1a644e7d242a250133f4644796685b
ecigwegbu <igwegbu@gmail.com>
2023-07-06 13:37:00 +0100

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""1. Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Write a type-annotated function concat that takes a string str1 and a
+    string str2 as arguments and returns a concatenated string"""
+    return ("{}{}".format(str1, str2))
