6bc763a491b32805e8684251939013cc964f888f
ecigwegbu <igwegbu@gmail.com>
2023-07-06 13:54:37 +0100

    2. Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""2. Basic annotations - floor"""
 
-"""0. Basic annotations - add"""
 
-
-def add(a: float, b: float) -> float:
-    """return the sum of a and b, both floats, as float."""
-    return (a + b)
+def floor(n: float) -> int:
+    """Write a type-annotated function floor which takes a float n as argument
+    and returns the floor of the float."""
+    if n >= 0:
+        return int(n)
+    return int(n) - 1
