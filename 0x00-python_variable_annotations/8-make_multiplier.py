ad28c13045b2fa7bc55b8d47a9a19adf86027ff1
ecigwegbu <igwegbu@gmail.com>
2023-07-06 15:57:51 +0100

    8. Complex types - functions

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+""""8. Complex types - functions"""
+from typing import Callable
 
-"""0. Basic annotations - add"""
 
-
-def add(a: float, b: float) -> float:
-    """return the sum of a and b, both floats, as float."""
-    return (a + b)
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Write a type-annotated function make_multiplier that takes a float
+    multiplier as argument and returns a function that multiplies a float
+    by multiplier."""
+    def cb(fl: float) -> float:
+        """multiply float by multiplier"""
+        return fl * multiplier
+    return cb
