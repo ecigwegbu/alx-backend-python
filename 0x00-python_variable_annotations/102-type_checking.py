a6b237042197ae9477d3e540bbd744d2d2556340
ecigwegbu <igwegbu@gmail.com>
2023-07-06 21:47:35 +0100

    12. Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
+"""12. Type Checking"""
+from typing import Tuple, List
 
-"""0. Basic annotations - add"""
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Use mypy to validate the following piece of code and apply any
+    necessary changes."""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
 
 
-def add(a: float, b: float) -> float:
-    """return the sum of a and b, both floats, as float."""
-    return (a + b)
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
