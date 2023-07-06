88e685479290028e3c1060350bd6278e611babaf
ecigwegbu <igwegbu@gmail.com>
2023-07-06 16:38:00 +0100

    11. More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+"""11. More involved type annotations"""
+import typing
 
-"""0. Basic annotations - add"""
 
-
-def add(a: float, b: float) -> float:
-    """return the sum of a and b, both floats, as float."""
-    return (a + b)
+def safely_get_value(dct, key, default=None):
+    """Given the parameters and the return values, add type annotations
+    to the function. Hint: look into TypeVar"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
