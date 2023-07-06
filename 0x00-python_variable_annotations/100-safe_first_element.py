54a8dee229f13e7d2981e76eb7418c160dbf8ffd
ecigwegbu <igwegbu@gmail.com>
2023-07-06 16:25:50 +0100

    10. Duck typing - first element of a sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+from typing import Any, Sequence, Union
 
-"""0. Basic annotations - add"""
+"""10. Duck typing - first element of a sequence"""
 
 
-def add(a: float, b: float) -> float:
-    """return the sum of a and b, both floats, as float."""
-    return (a + b)
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, type(None)]:
+    """Augument code with duck-typing"""
+    if lst:
+        return lst[0]
+    else:
+        return None
