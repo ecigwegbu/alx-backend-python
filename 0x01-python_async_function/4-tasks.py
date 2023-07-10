7d30d832f3d41820dd8aeaca69182f9e72204503
ecigwegbu <igwegbu@gmail.com>
2023-07-10 18:23:50 +0100

    4.Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 """
-Write an asynchronous coroutine that takes in an integer argument (max_delay,
-with a default value of 10) named wait_random that waits for a random delay
-between 0 and max_delay (included and float value) seconds and eventually
-returns it.
-
-Use the random module.
+4. Tasks:
+Let's execute multiple coroutines at the same time with async
 """
 import asyncio
-import random
+import typing
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """wait max of max_delay seconds and return the time waited."""
-    wait: float = random.random() * max_delay
-    await asyncio.sleep(wait)
-    return wait
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """Take the code from wait_n and alter it into a new function
+    task_wait_n. The code is nearly identical to wait_n except
+    task_wait_random is being called:
+
+    Import wait_random from the previous python file that you’ve written
+    and write an async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay. You will spawn wait_random n times
+    with the specified max_delay.
+
+    wait_n should return the list of all the delays (float values). The list
+    of the delays should be in ascending order without using sort() because
+    of concurrency."""
+
+    #  spawn wait_random n times:
+    result: list[float] = await asyncio.gather(*[task_wait_random(max_delay)
+                                                 for _ in range(n)])
+    return sorted(result)
 
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) < 2:
-        print('Usage: {} <number_of_seconds>'.format(sys.argv[0]))
-        exit(98)
-    print('Sleeping...\n')
-    wait_random(int(sys.argv[1]))
+        pass
+        #  print('Usage: {} <number_of_seconds>'.format(sys.argv[0]))
+        #  exit(98)
+    #  code here
+    pass
