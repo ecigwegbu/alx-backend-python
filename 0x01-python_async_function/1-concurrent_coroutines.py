4fa9e27765ea3c82597bebb2c6e765a3e826e7ca
ecigwegbu <igwegbu@gmail.com>
2023-07-10 13:58:55 +0100

    1. Let's execute multiple coroutines at the same time with async - setup

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def wait_n(n: int, max_delay: int) -> typing.List[foat]:
+async def wait_n(n: int, max_delay: int) -> [float]:
     """Import wait_random from the previous python file that you’ve written
     and write an async routine called wait_n that takes in 2 int arguments
     (in this order): n and max_delay. You will spawn wait_random n times
