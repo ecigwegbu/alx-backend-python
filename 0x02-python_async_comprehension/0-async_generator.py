cc69fbc51c5d77f4b9258dad41f09461d07de18b
ecigwegbu <igwegbu@gmail.com>
2023-07-11 22:30:30 +0100

    0.Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 import typing
 
 
-async def async_generator() -> typing.AsyncGenerator[float, type[None]]:
+async def async_generator() -> typing.AsyncGenerator[float, float]:
     """Write a coroutine called async_generator that takes no arguments.
     The coroutine will loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10. Use the random module."""
