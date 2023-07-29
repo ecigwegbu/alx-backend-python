951e4bf7e3484bb7d0c9a1cfdfeb4bd42d8ccf3d
ecigwegbu <igwegbu@gmail.com>
2023-07-29 18:29:04 +0000

    5. Mocking a property, Pass instead of pass!

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,17 +9,12 @@ from unittest.mock import Mock, patch, PropertyMock
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test that GithubOrgClient.org returns the correct value.
-    Use patch as a decorator to make sure get_json is called once with the
-    Use patch as a decorator to make sure get_json is called once with the
+
+    Use @patch as a decorator to make sure get_json is called once with the
     expected argument but make sure it is not executed.
-    Use parameterized.expand as a decorator to parametrize the test with a
-    expected argument but make sure it is not executed.
-    Use parameterized.expand as a decorator to parametrize the test with a
-    expected argument but make sure it is not executed.
-    Use parameterized.expand as a decorator to parametrize the test with a
-    expected argument but make sure it is not executed.
-    Use parameterized.expand as a decorator to parametrize the test with a
-    couple of org examples to passs to GithubOrgClient, in this order
+
+    Use @parameterized.expand as a decorator to parametrize the test with a
+    couple of org examples to Pass to GithubOrgClient, in this order:
 
     google
     abc
