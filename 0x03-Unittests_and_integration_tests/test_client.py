869758a4a25e9afdd09a7138b35ceb0b5f0be476
ecigwegbu <igwegbu@gmail.com>
2023-07-29 18:27:09 +0000

    no blanks and indented - comment changed - no blanks - no at sign no google abc - shortened - lengthened-much added google abc with blank - added line without colon with passs

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -19,7 +19,7 @@ class TestGithubOrgClient(unittest.TestCase):
     Use parameterized.expand as a decorator to parametrize the test with a
     expected argument but make sure it is not executed.
     Use parameterized.expand as a decorator to parametrize the test with a
-    couple of org examples to Passs to GithubOrgClient, in this order
+    couple of org examples to passs to GithubOrgClient, in this order
 
     google
     abc
